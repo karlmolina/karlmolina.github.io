97b9aee2607317057ce4976ce34c1327b46dc4f4
Karl Molina <karlgmolina@gmail.com>
2019-12-25 01:41:55 -0700

    Remove link to slinky monster 2

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ mySketch = new p5(sketch => {
         links = [];
         links.push(new Link('Connected', 'connected'));
         links.push(new Link('Slinky Monster', 'slinky_monster'));
-        links.push(new Link('Slinky Monster 2', 'slinky_monster_2'));
+        // links.push(new Link('Slinky Monster 2', 'slinky_monster_2'));
         links.push(new Link('Tic Tac Toe', 'tictactoe'));
         // sketch.background(0);
     }
