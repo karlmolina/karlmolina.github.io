f5f6ff59fc49bc6701abefd766096c44d09a8559
Karl Molina <karlgmolina@gmail.com>
2019-12-29 10:20:29 -0700

    Fix window resize bug

--- utils/sketchUtils.mjs
+++ utils/sketchUtils.mjs
@@ -1,12 +1,22 @@
+function resizeWindow(s) {
+    s.windowWidth = s.windowWidth / 2;
+    s.windowHeight = s.windowHeight / 2;
+}
+
 const sketchUtils = {
     isTouchDevice: 'ontouchstart' in document.documentElement,
     wrapSketch: function (wrappedFunction) {
         return s => {
             if (sketchUtils.isTouchDevice) {
-                s.windowWidth = s.windowWidth / 2;
-                s.windowHeight = s.windowHeight / 2;
+                resizeWindow(s);
             }
             wrappedFunction(s);
+
+            const wrappedWindowResized = s.windowResized;
+            s.windowResized = () => {
+                resizeWindow(s);
+                wrappedWindowResized();
+            };
         };
     }
 };
