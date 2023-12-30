0427278144f508e3ad87f18c869b72134da39dd9
Karl Molina <karlgmolina@gmail.com>
2023-12-30 01:04:01 -0700

    Add connected

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,77 @@
 import './style.css'
 
-import viteLogo from '/vite.svg'
+import { html } from 'htl'
+import Navigo from 'navigo'
+import p5 from 'p5'
 
-import { setupCounter } from './counter.ts'
-import typescriptLogo from './typescript.svg'
+import { $ } from './lib/html-utils.ts'
+import { setAppContent } from './lib/set-app-content.ts'
+import connected from './sketches/connected.ts'
+import tornadoHole from './sketches/tornado-hole.ts'
+import tree from './sketches/tree.ts'
+import sketchUtils from './utils/sketch-utils.ts'
 
+const main = new Navigo('/', { hash: true })
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const sketchList = ['connected', 'slinky monster', 'tree', 'tornado hole']
+const p5Sketches = {
+  connected: connected,
+  'slinky monster': connected,
+  tree: tree,
+}
+let sketch: p5 | undefined
+let resize: () => void
+main
+  .hooks({
+    before: (done) => {
+      sketch?.remove()
+      resize && window.removeEventListener('resize', resize)
+      done()
+    },
+  })
+  .on(() => {
+    const link = (name: string) =>
+      html`<li class="my-2 sm:my-4">
+        <a
+          class="inline-block w-full text-2xl hover:bg-teal-100 sm:text-4xl"
+          href="#/${name}"
+          >${name}</a
+        >
+      </li>`
+    setAppContent(html`
+      <div class="m-auto max-w-screen-lg px-8 pt-16 sm:px-16 sm:pt-24">
+        <h1
+          class="
+          border-b-0.05em mb-1 mt-2 box-border break-words border-teal-500 font-[questrial] text-4xl font-normal tracking-tighter text-[#1A936F] underline sm:text-6xl md:text-8xl"
+        >
+          karlmolina.com
+        </h1>
+        <ul class="font-barlow my-4 list-none text-left text-[#114b5f]">
+          ${sketchList.map((name) => link(name))}
+        </ul>
+      </div>
+    `)
+  })
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+for (const [name, sketchFunction] of Object.entries(p5Sketches)) {
+  // url encode name
+  const encodedName = name.replace(/ /g, '%20')
+  main.on(`/${encodedName}`, () => {
+    document.title = name
+    setAppContent(html`<div id="canvas"></div>`)
+    sketch = new p5(sketchUtils.wrapSketch(sketchFunction), $('canvas'))
+  })
+}
+main.on('/tornado%20hole', () => {
+  document.title = 'tornado hole'
+  let app = tornadoHole()
+  document.body.replaceChildren(app.view)
+  resize = () => {
+    app.destroy(true)
+    app = tornadoHole()
+    document.body.replaceChildren(app.view)
+  }
+  window.addEventListener('resize', resize)
+})
+main.resolve()
+export default main
--- src/utils/sketch-utils.ts
+++ src/utils/sketch-utils.ts
@@ -1,116 +1,111 @@
-import p5 from 'p5';
+import p5, { Vector } from 'p5'
 
 function getWindowDimensions(s: Sketch) {
-  const ratio = s.windowWidth / s.windowHeight;
+  const ratio = s.windowWidth / s.windowHeight
 
   let width = s.windowWidth,
-    height = s.windowHeight;
+    height = s.windowHeight
 
-  const maxDimension = 650;
+  const maxDimension = 650
   if (sketchUtils.isTouchDevice) {
     if (height > width) {
-      height = maxDimension;
-      width = height * ratio;
+      height = maxDimension
+      width = height * ratio
     } else {
-      width = maxDimension;
-      height = width / ratio;
+      width = maxDimension
+      height = width / ratio
     }
   }
 
-  return [width, height];
+  return [width, height]
 }
 
 function showDebugInfo(s: Sketch, pg: Sketch, maxWidth: number) {
-  pg.clear();
+  pg.clear()
   const debugInfo = [
     'fps: ' + Math.round(s.frameRate()),
     'width: ' + s.width,
     'height: ' + s.height,
     'mouseX: ' + Math.round(s.mouseX),
     'mouseY: ' + Math.round(s.mouseY),
-  ];
+  ]
 
   debugInfo.forEach((info) => {
-    const width = pg.textWidth(info);
+    const width = pg.textWidth(info)
     if (width > maxWidth) {
-      maxWidth = width;
+      maxWidth = width
     }
-  });
-  pg.push();
-  pg.translate(10, 10);
+  })
+  pg.push()
+  pg.translate(10, 10)
 
-  pg.rect(
-    0,
-    0,
-    Math.round(maxWidth) + 10,
-    pg.textSize() * debugInfo.length + 5,
-  );
+  pg.rect(0, 0, Math.round(maxWidth) + 10, pg.textSize() * debugInfo.length + 5)
 
   for (let i = 0; i < debugInfo.length; i++) {
-    const info = debugInfo[i];
-    pg.text(info, 5, (i + 1) * pg.textSize());
+    const info = debugInfo[i]
+    pg.text(info, 5, (i + 1) * pg.textSize())
   }
 
-  pg.pop();
+  pg.pop()
 
-  s.image(pg, 0, 0);
+  s.image(pg, 0, 0)
 }
 
 function getCenter(s: Sketch) {
-  return s.createVector(s.width / 2, s.height / 2);
+  return s.createVector(s.width / 2, s.height / 2)
 }
 
 export interface Sketch extends p5 {
-  updateSketch: () => void;
+  updateSketch: () => void
 }
 
 const sketchUtils = {
   isTouchDevice: 'ontouchstart' in document.documentElement,
-  center: {},
+  center: Vector.fromAngle(0),
   minSize: 0,
   wrapSketch: function (wrappedSketchFunction: (s: Sketch) => void) {
     return (s: Sketch) => {
-      wrappedSketchFunction(s);
+      wrappedSketchFunction(s)
 
       // Allow us to call updateSketch when it doesn't exist.
-      const wrappedUpdateSketch = s.updateSketch;
+      const wrappedUpdateSketch = s.updateSketch
       s.updateSketch = () => {
-        sketchUtils.center = getCenter(s);
-        sketchUtils.minSize = Math.min(s.width, s.height);
+        sketchUtils.center = getCenter(s)
+        sketchUtils.minSize = Math.min(s.width, s.height)
 
         if (wrappedUpdateSketch) {
-          wrappedUpdateSketch();
+          wrappedUpdateSketch()
         }
-      };
+      }
 
-      const wrappedSetup = s.setup;
+      const wrappedSetup = s.setup
       s.setup = () => {
-        const [width, height] = getWindowDimensions(s);
-        s.createCanvas(width, height);
+        const [width, height] = getWindowDimensions(s)
+        s.createCanvas(width, height)
 
-        wrappedSetup();
-        s.updateSketch();
-      };
+        wrappedSetup()
+        s.updateSketch()
+      }
 
-      const wrappedWindowResized = s.windowResized;
+      const wrappedWindowResized = s.windowResized
       s.windowResized = () => {
-        const [width, height] = getWindowDimensions(s);
-        s.resizeCanvas(width, height);
+        const [width, height] = getWindowDimensions(s)
+        s.resizeCanvas(width, height)
 
-        s.updateSketch();
+        s.updateSketch()
 
         if (wrappedWindowResized) {
-          wrappedWindowResized();
+          wrappedWindowResized()
         }
-      };
+      }
 
-      const wrappedDraw = s.draw;
+      const wrappedDraw = s.draw
       s.draw = () => {
-        wrappedDraw();
+        wrappedDraw()
         // showDebugInfo(s, pg, maxWidth);
-      };
-    };
+      }
+    }
   },
-};
+}
 
-export default sketchUtils;
+export default sketchUtils
